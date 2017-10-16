386c35987b1c7ff19a6aa4b41c2c3095a1f84b21
minzhejiang <30670834+minzhejiang@users.noreply.github.com>
2017-10-16 17:13:55 -0400

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,5 +1,5 @@
-var img1, img2, img3, img4, img5, img6,img7,img8,img9,img10,img11;
-var portraits = [img1, img2, img3, img4, img5, img6,img7,img8,img9,img10,img11];
+var portret = [];
+var img1, img2, img3, img4, img5;
 var hold=false;
 var blured=false;
 function preload() {
@@ -8,12 +8,6 @@ function preload() {
   img3 = loadImage("pic3.jpg");
   img4 = loadImage("pic4.jpg");
   img5 = loadImage("pic5.jpg");
-  img6 = loadImage("pic6.jpg");
-  img7 = loadImage("pic7.jpg");
-  img8 = loadImage("pic8.jpg");
-  img9 = loadImage("pic9.jpg");
-  img10 = loadImage("pic10.jpg");
-  img11 = loadImage("pic11.jpg");
   
 }
 function setup() {
@@ -23,13 +17,11 @@ function setup() {
   img3.loadPixels();
   img4.loadPixels();
   img5.loadPixels();
-  img6.loadPixels();
-  img7.loadPixels();
-  img8.loadPixels();
-  img9.loadPixels();
-  img10.loadPixels();
-  img11.loadPixels();
-
+  portret.push(img1);
+  portret.push(img2);
+  portret.push(img3);
+  portret.push(img4);
+  portret.push(img5); 
   loadPixels();
 }
 
@@ -38,10 +30,10 @@ function draw() {
   for (var i = 0; i < 20000; i++) {
     var x = int(random(width));
     var y = int(random(height));
-    var nimg=int(10*noise(x*.002+cos(frameCount*.0001), y*.002+sin(frameCount*.001), sin(frameCount*.01)));
+    var nimg=int(5*noise(x*.002+cos(frameCount*.0001), y*.002+sin(frameCount*.001), sin(frameCount*.01)));
     for (var k = 0; k < 4; k++) {
       var col=pixels[k + 4 * (y * width + x)] ;
-      pixels[k + 4 * (y * width + x)] =int(.5*portraits[nimg].pixels[k+4* (y * width + x)]+.5*col);
+      pixels[k + 4 * (y * width + x)] =int(.5*portret[nimg].pixels[k+4* (y * width + x)]+.5*col);
     }
   }
   if(!hold)updatePixels();
